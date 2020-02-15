4c9456a4b3c551f57ce4d7f4eb0d62e1b8cde86f
Junaid Ashraf <56659999+ash-junaid@users.noreply.github.com>
2020-02-16 02:08:57 +1100

    Add files via upload

---
+++ GHphp/es3.php
@@ -0,0 +1,23 @@
+<?php
+$name1 = "Percy Jackk";
+$email1 = "percy.jackson2290@gmail.com";
+$curl = curl_init();
+curl_setopt_array($curl, array(
+    CURLOPT_URL => "https://demo.docusign.net/restapi/v2.1/accounts/7145655/envelopes",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "POST",
+    CURLOPT_POSTFIELDS => json_encode(["emailSubject" => "DocuSignAPI-CompositeTemplates", "emailBlurb" => "CompositeTemplatesSample1", "status" => "sent", "compositeTemplates" => ["serverTemplates" => ["sequence" => "1", "templateId" => "ff32490f-58ab-4e26-a505-b32c863b2398"]], ["inlineTemplates" => ["sequence" => "1", "recipients" => ["signers" => ["email" => $email1, "name" => $name1, "recipientId" => "1", "roleName" => "Client"]]]]]),
+    CURLOPT_HTTPHEADER => array(
+        "X-DocuSign-Authentication: {\"Username\":\"stewie.griffin2290@gmail.com\",\"Password\":\"0lWiibvA9gsWYIZu8AnOKZSSYDw=\",\"IntegratorKey\": \"ea059d7d-8c39-4e2a-a17a-cb2079165cd6\"}",
+        "Content-Type: application/json"
+    )
+));
+$response = curl_exec($curl);
+curl_close($curl);
+echo $response;
+?>
---
+++ GHphp/formphp.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+
+$name1 = $_POST["fname1"];
+$email1 = $_POST["femail1"];
+$age1 = $_POST["fage1"];
+$address1 = $_POST["fadress1"];
+$cnum = $_POST["fcnum1"];
+
+echo $name1;
+?>
+<br><?php
+echo $email1;
+?>
+<br><?php
+echo $age1;
+?>
+<br> <?php
+echo $address1;
+?>
+<br> <?php
+echo $cnum;
+?>
+
+</body>
+</html>
