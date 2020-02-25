ac1c0d710780c05d642af2fea8ac9ce38eb5c7b4
ash-junaid <ash-junaid@users.noreply.github.com>
2020-02-25 11:44:10 +1100

    Postman_environment issue resolved

--- GHphp/temb.php
+++ GHphp/temb.php
@@ -3,15 +3,15 @@
 $name1 = $_POST["fname1"];
 $email1 = $_POST["femail1"];
 $age1 = $_POST["fage1"];
-$address1 = $_POST["fadress1"];
+$address1 = $_POST["faddress1"];
 $cnum = $_POST["fcnum1"];
 
-<?php
+
 
 $curl = curl_init();
 
 curl_setopt_array($curl, array(
-  CURLOPT_URL => "%7B%7BbaseUrl%7D%7D/envelopes",
+  CURLOPT_URL => "https://demo.docusign.net/restapi/v2.1/accounts/7145655/envelopes",
   CURLOPT_RETURNTRANSFER => true,
   CURLOPT_ENCODING => "",
   CURLOPT_MAXREDIRS => 10,
@@ -19,9 +19,9 @@ curl_setopt_array($curl, array(
   CURLOPT_FOLLOWLOCATION => true,
   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
   CURLOPT_CUSTOMREQUEST => "POST",
-  CURLOPT_POSTFIELDS =>"{\n    \"accountId\": \"7145655\",\n    \"emailSubject\": \"API Example - Populating Data in Templates\",\n    \"templateId\": \"b854e0ee-7c6b-47ac-b80a-b60375be08ed\",\n    \"templateRoles\": [{\n        \"email\": \"{$email1}\",\n        \"name\": \"{$name1}\",\n        \"roleName\": \"Member\",\n        \"tabs\": {\n            \"textTabs\": [{\n                \"tabLabel\": \"age\",\n                \"value\": \"{$age}\"\n            }, \n            {\n                \"tabLabel\": \"caddress\",\n                \"value\": \"{$address1}\"\n            }]\n        }\n    }],\n    \"status\": \"sent\"\n}",
+  CURLOPT_POSTFIELDS =>"{\n    \"accountId\": \"7145655\",\n    \"emailSubject\": \"API Example - Populating Data in Templates\",\n    \"templateId\": \"b854e0ee-7c6b-47ac-b80a-b60375be08ed\",\n    \"templateRoles\": [{\n        \"email\": \"{$email1}\",\n        \"name\": \"{$name1}\",\n        \"roleName\": \"Member\",\n        \"tabs\": {\n            \"textTabs\": [{\n                \"tabLabel\": \"age\",\n                \"value\": \"{$age1}\"\n            }, \n            {\n                \"tabLabel\": \"caddress\",\n                \"value\": \"{$address1}\"\n            }]\n        }\n    }],\n    \"status\": \"sent\"\n}",
   CURLOPT_HTTPHEADER => array(
-    "X-DocuSign-Authentication: {\"Username\":\"{{username}}\",\"Password\":\"{{password}}\",\"IntegratorKey\": \"{{clientId}}\"}",
+    "X-DocuSign-Authentication: {\"Username\":\"stewie.griffin2290@gmail.com\",\"Password\":\"0lWiibvA9gsWYIZu8AnOKZSSYDw=\",\"IntegratorKey\": \"ea059d7d-8c39-4e2a-a17a-cb2079165cd6\"}",
     "Content-Type: application/json"
   ),
 ));
@@ -30,3 +30,4 @@ $response = curl_exec($curl);
 
 curl_close($curl);
 echo $response;
+?>
