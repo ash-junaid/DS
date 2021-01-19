8226c2a24bca7b8ce2c85ed731b743d3aea00828
ash-junaid <ash-junaid@users.noreply.github.com>
2021-01-19 15:30:52 +1100

    Update sfdc_sign.php

--- GHphp/SFDC/sfdc_sign.php
+++ GHphp/SFDC/sfdc_sign.php
@@ -35,7 +35,7 @@ $resp2=$pieces2[0];
 ////echo $resp2;
 $curl = curl_init();
 $uri2 = "https://demo.docusign.net/restapi//v2.1/accounts/4fbaccb3-876d-42f1-bac3-5677d9067c5f/envelopes/".$resp2."/views/recipient";
-echo $uri2;
+//echo $uri2;
 curl_setopt_array($curl, array(
   CURLOPT_URL => $uri2,
   CURLOPT_RETURNTRANSFER => true,
@@ -48,7 +48,8 @@ curl_setopt_array($curl, array(
   CURLOPT_POSTFIELDS =>"{\n  \"returnUrl\": \"http://localhost/returnUrl\",\n  \"authenticationMethod\": \"None\",\n  \"email\": \"{$email1}\",\n  \"userName\": \"{$name1}\",\n  \"clientUserId\": 1\n}",
 
   CURLOPT_HTTPHEADER => array(
-    'X-DocuSign-Authentication: {"Username":"{{username}}","Password":"{{password}}","IntegratorKey": "{{clientId}}"}',
+    'Accept: application/json',
+    'Authorization: Bearer eyJ0eXAiOiJNVCIsImFsZyI6IlJTMjU2Iiwia2lkIjoiNjgxODVmZjEtNGU1MS00Y2U5LWFmMWMtNjg5ODEyMjAzMzE3In0.AQoAAAABAAUABwAACMidIbzYSAgAAEjrq2S82EgCAM2-DigiKy1Lk5aiHJOP1HsVAAEAAAAYAAEAAAAFAAAADQAkAAAANTBiZGI4OTItOWM3Yi00NGNkLWI2Y2MtZDk0YTg2Y2U2MzJhIgAkAAAANTBiZGI4OTItOWM3Yi00NGNkLWI2Y2MtZDk0YTg2Y2U2MzJhMAAA5J0uILzYSDcA4C-CYxk0m0W2Buyqt8xSSg.af5w0o6zujfoJ68hhbztmLjGc_pySXZ8eiur4rXyWxnF9A1QvXLcIzqf0HwUIlbNHTQPI8cs99Aq88pLn2IeFXP4nhIq0d5ISUJRTpzfvZQtyC8ZCEhSUrSGjWmTsMY1dpkhoZol44yYMv-FxL-O4rqVoCHL9oyDraFjYWTtHLEA3Qz3PQdqRFSUR-7bO2WuDSOh4qJl473CcZIRrUepUKjitBDDFCInhoiKMfXYMwNCYGCqsBG_Po7rHj4SE-cXs9rBIL6wJeM9N1jMmVFohr-VPuCebyPIJXpGyJcYsG2zGi88by7A1MpkvkG1jDdKnudb3-3m2Jx5-GQ9hBMlFw',
     'Content-Type: application/json',
     'Cookie: acctLogin=True; BIGipDocuSign_Demo=!0JujKagN/XbCe4SU4V8NQL3lsxkc/fTyqOu+sv0AjvzK6IKTwNm4dgnZgXgyKRXwYeS1nZ0/N+J0zAA='
   ),
@@ -57,6 +58,11 @@ curl_setopt_array($curl, array(
 $response = curl_exec($curl);
 
 curl_close($curl);
-//echo $response;
+$resp3 = explode("\"", $response);
+$sign_url=$resp3[3];
+
+//echo "$response_url_temp";
+//header("Location: $sign_url");
+echo $sign_url
 
 ?>
