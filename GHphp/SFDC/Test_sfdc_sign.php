c855f19a9cd93a1b6bad69846239d929f093e874
ash-junaid <ash-junaid@users.noreply.github.com>
2021-01-20 10:15:56 +1100

    Update Test_sfdc_sign.php

--- GHphp/SFDC/Test_sfdc_sign.php
+++ GHphp/SFDC/Test_sfdc_sign.php
@@ -1,4 +1,3 @@
-
 <?php
 $name1 = $_POST["fname1"];
 $email1 = $_POST["femail1"];
@@ -18,7 +17,7 @@ curl_setopt_array($curl, array(
   CURLOPT_POSTFIELDS =>"{\n    \"accountId\": \"10899188\",\n    \"emailSubject\": \"API Example - Populating Data SFDC\",\n    \"templateId\": \"369e0018-da77-4050-a8df-db0d77f21f4c\",\n    \"templateRoles\": [{\n        \"email\": \"{$email1}\",\n        \"name\": \"{$name1}\",\n        \"clientUserId\": 1,\n        \"roleName\": \"Customer\",\n        \"tabs\": {\n            \"textTabs\": [{\n                \"tabLabel\": \"Current Address\",\n                \"value\": \"{$address1}\"\n            }, \n            {\n                \"tabLabel\": \"ABN\",\n                \"value\": \"{$age1}\"\n            }]\n        }\n    }],\n    \"status\": \"sent\"\n}",
   CURLOPT_HTTPHEADER => array(
     'Accept: application/json',
-    'Authorization: Bearer eyJ0eXAiOiJNVCIsImFsZyI6IlJTMjU2Iiwia2lkIjoiNjgxODVmZjEtNGU1MS00Y2U5LWFmMWMtNjg5ODEyMjAzMzE3In0.AQoAAAABAAUABwAACMidIbzYSAgAAEjrq2S82EgCAM2-DigiKy1Lk5aiHJOP1HsVAAEAAAAYAAEAAAAFAAAADQAkAAAANTBiZGI4OTItOWM3Yi00NGNkLWI2Y2MtZDk0YTg2Y2U2MzJhIgAkAAAANTBiZGI4OTItOWM3Yi00NGNkLWI2Y2MtZDk0YTg2Y2U2MzJhMAAA5J0uILzYSDcA4C-CYxk0m0W2Buyqt8xSSg.af5w0o6zujfoJ68hhbztmLjGc_pySXZ8eiur4rXyWxnF9A1QvXLcIzqf0HwUIlbNHTQPI8cs99Aq88pLn2IeFXP4nhIq0d5ISUJRTpzfvZQtyC8ZCEhSUrSGjWmTsMY1dpkhoZol44yYMv-FxL-O4rqVoCHL9oyDraFjYWTtHLEA3Qz3PQdqRFSUR-7bO2WuDSOh4qJl473CcZIRrUepUKjitBDDFCInhoiKMfXYMwNCYGCqsBG_Po7rHj4SE-cXs9rBIL6wJeM9N1jMmVFohr-VPuCebyPIJXpGyJcYsG2zGi88by7A1MpkvkG1jDdKnudb3-3m2Jx5-GQ9hBMlFw',
+    'Authorization: Bearer eyJ0eXAiOiJNVCIsImFsZyI6IlJTMjU2Iiwia2lkIjoiNjgxODVmZjEtNGU1MS00Y2U5LWFmMWMtNjg5ODEyMjAzMzE3In0.AQoAAAABAAUABwCAmkKXzLzYSAgAgNplpQ-92EgCAM2-DigiKy1Lk5aiHJOP1HsVAAEAAAAYAAEAAAAFAAAADQAkAAAANTBiZGI4OTItOWM3Yi00NGNkLWI2Y2MtZDk0YTg2Y2U2MzJhIgAkAAAANTBiZGI4OTItOWM3Yi00NGNkLWI2Y2MtZDk0YTg2Y2U2MzJhMACAgbB0zLzYSDcA4C-CYxk0m0W2Buyqt8xSSg.mdaeflfMBJfIXMguvjjdPMrP1f0jVInkNGU357BY4KNm6aHlzRv3AdcAvb48FQjboBMSFqr8_g3wHFqrs9OjTt6GnXeIn-52MpHemTdBRA_UMrkVBkHUqK3l0YZ6fm3EFwKrfSVAFjNQtkIOeasRqLR_6nmKTZ37QU86GnEbL72gJ34_1RdxM8YbWoqlji4Yk2887nbaiojrgJKgNcAJVm_t446cDzbdFJ5BNcAUfCrJHHJvn0naQKt8zz7l6Wvv-X9eDqmsUiTNtzDH10BOGP3DFViOg8y_qX98W1q4u2rLl24rgkcbXw0SwITebYlEhM2M0lrIN-_ukSJyP25ohA',
     'Content-Type: application/json',
     'Cookie: acctLogin=True; BIGipDocuSign_Demo=!0JujKagN/XbCe4SU4V8NQL3lsxkc/fTyqOu+sv0AjvzK6IKTwNm4dgnZgXgyKRXwYeS1nZ0/N+J0zAA='
   ),
@@ -48,9 +47,9 @@ curl_setopt_array($curl, array(
   CURLOPT_CUSTOMREQUEST => "POST",
   CURLOPT_POSTFIELDS =>"{\n  \"returnUrl\": \"http://localhost/returnUrl\",\n  \"authenticationMethod\": \"None\",\n  \"email\": \"{$email1}\",\n  \"userName\": \"{$name1}\",\n  \"clientUserId\": 1\n}",
 
-  CURLOPT_HTTPHEADER => array(
+ CURLOPT_HTTPHEADER => array(
     'Accept: application/json',
-    'Authorization: Bearer eyJ0eXAiOiJNVCIsImFsZyI6IlJTMjU2Iiwia2lkIjoiNjgxODVmZjEtNGU1MS00Y2U5LWFmMWMtNjg5ODEyMjAzMzE3In0.AQoAAAABAAUABwAACMidIbzYSAgAAEjrq2S82EgCAM2-DigiKy1Lk5aiHJOP1HsVAAEAAAAYAAEAAAAFAAAADQAkAAAANTBiZGI4OTItOWM3Yi00NGNkLWI2Y2MtZDk0YTg2Y2U2MzJhIgAkAAAANTBiZGI4OTItOWM3Yi00NGNkLWI2Y2MtZDk0YTg2Y2U2MzJhMAAA5J0uILzYSDcA4C-CYxk0m0W2Buyqt8xSSg.af5w0o6zujfoJ68hhbztmLjGc_pySXZ8eiur4rXyWxnF9A1QvXLcIzqf0HwUIlbNHTQPI8cs99Aq88pLn2IeFXP4nhIq0d5ISUJRTpzfvZQtyC8ZCEhSUrSGjWmTsMY1dpkhoZol44yYMv-FxL-O4rqVoCHL9oyDraFjYWTtHLEA3Qz3PQdqRFSUR-7bO2WuDSOh4qJl473CcZIRrUepUKjitBDDFCInhoiKMfXYMwNCYGCqsBG_Po7rHj4SE-cXs9rBIL6wJeM9N1jMmVFohr-VPuCebyPIJXpGyJcYsG2zGi88by7A1MpkvkG1jDdKnudb3-3m2Jx5-GQ9hBMlFw',
+    'Authorization: Bearer eyJ0eXAiOiJNVCIsImFsZyI6IlJTMjU2Iiwia2lkIjoiNjgxODVmZjEtNGU1MS00Y2U5LWFmMWMtNjg5ODEyMjAzMzE3In0.AQoAAAABAAUABwCAmkKXzLzYSAgAgNplpQ-92EgCAM2-DigiKy1Lk5aiHJOP1HsVAAEAAAAYAAEAAAAFAAAADQAkAAAANTBiZGI4OTItOWM3Yi00NGNkLWI2Y2MtZDk0YTg2Y2U2MzJhIgAkAAAANTBiZGI4OTItOWM3Yi00NGNkLWI2Y2MtZDk0YTg2Y2U2MzJhMACAgbB0zLzYSDcA4C-CYxk0m0W2Buyqt8xSSg.mdaeflfMBJfIXMguvjjdPMrP1f0jVInkNGU357BY4KNm6aHlzRv3AdcAvb48FQjboBMSFqr8_g3wHFqrs9OjTt6GnXeIn-52MpHemTdBRA_UMrkVBkHUqK3l0YZ6fm3EFwKrfSVAFjNQtkIOeasRqLR_6nmKTZ37QU86GnEbL72gJ34_1RdxM8YbWoqlji4Yk2887nbaiojrgJKgNcAJVm_t446cDzbdFJ5BNcAUfCrJHHJvn0naQKt8zz7l6Wvv-X9eDqmsUiTNtzDH10BOGP3DFViOg8y_qX98W1q4u2rLl24rgkcbXw0SwITebYlEhM2M0lrIN-_ukSJyP25ohA',
     'Content-Type: application/json',
     'Cookie: acctLogin=True; BIGipDocuSign_Demo=!0JujKagN/XbCe4SU4V8NQL3lsxkc/fTyqOu+sv0AjvzK6IKTwNm4dgnZgXgyKRXwYeS1nZ0/N+J0zAA='
   ),
@@ -61,27 +60,9 @@ $response = curl_exec($curl);
 curl_close($curl);
 $resp3 = explode("\"", $response);
 $sign_url=$resp3[3];
-
+<iframe src="<?php echo $sign_url; ?>" width="100%" height="100%" ></iframe>
 //echo "$response_url_temp";
-
 //header("Location: $sign_url");
-//echo $sign_url
+//echo $sign_url;
 
 ?>
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>My Example</title>
-        <link rel="stylesheet" href="styles.css">
-    </head>
-    <body>
-     <h2>APAC SE - Embedded Signing to Salesforce</h2>
-
-     <h3> Please fill in the folloiwng details to start signing: </h3>
-
-
-
-    </body>
-
-<iframe src="https://www.google.com/" width="100%" height="500%" ></iframe>
-</html>
