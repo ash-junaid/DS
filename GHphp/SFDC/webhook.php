13325c904b9046a20595ac77015b1327373fccce
Junaid Ashraf <56659999+ash-junaid@users.noreply.github.com>
2021-06-24 21:59:37 +1000

    No changes

---
+++ GHphp/SFDC/webhook.php
@@ -0,0 +1,21 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // fetch RAW input
+    $json = file_get_contents('php://input');
+
+    // decode json
+    $object = json_decode($json);
+
+    // expecting valid json
+    if (json_last_error() !== JSON_ERROR_NONE) {
+        die(header('HTTP/1.0 415 Unsupported Media Type'));
+    }
+
+    /**
+     * Do something with object, structure will be like:
+     * $object->accountId
+     * $object->details->items[0]['contactName']
+     */
+    // dump to file so you can see
+    file_put_contents('callback.test.txt', print_r($object, true));
+}
